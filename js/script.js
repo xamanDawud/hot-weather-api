d4ced8960fe1c7212ca4039bc396b44af45b2830
xamanDawud <ahmeddaudking100@gmail.com>
2022-09-07 11:50:43 +0600

    search to get any city weatehr project completed

--- js/script.js
+++ js/script.js
@@ -2,16 +2,26 @@ const api_key = "effb4d4d2b170e3961e84cc65f7d760f";
 
 let loadWeatherData = (city) => {
     fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
         )
         .then((res) => res.json())
         .then((data) => displayData(data));
 };
 
 const displayData = (data) => {
-    let cityName = document.getElementById("cityName");
-    cityName.innerText = data.name;
+    innerTextbyId("cityName", data.name);
+    innerTextbyId("temp", data.main.temp);
+    innerTextbyId("cloud", data.weather[0].main);
     // console.log(data.name);
 };
 
-loadWeatherData("london");
+function innerTextbyId(id, textValue) {
+    let idd = document.getElementById(id);
+    idd.innerText = textValue;
+}
+document.getElementById("search-btn").addEventListener("click", function() {
+    let searchInput = document.getElementById("serarch-input");
+    let searchValue = searchInput.value;
+    loadWeatherData(searchValue);
+    // console.log(searchValue);
+});
